8c076f6e738d19337f52b0429c85fc0ca3f12697
Alejandro Arguello <daw19.2021@gmail.com>
2021-10-04 21:39:19 +0200

    Implements routes and lazyload (lesson 331 course angular)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GraficasRoutingModule} from "./graficas/graficas-routing.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'graficas',
+    loadChildren: () => import('./graficas/graficas.module').then((m) => m.GraficasModule)
+  },
+{
+  path: '**',
+  redirectTo: 'graficas'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
