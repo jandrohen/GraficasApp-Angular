2d93cf1d7333fc6cb7ee8fad512068130222c010
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-06 17:14:31 +0200

    implement customised component for displaying graphics (lesson 335 course angular)

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ChartDataSets, ChartOptions, ChartType} from "chart.js";
+import {Label} from "ng2-charts";
 
 @Component({
   selector: 'app-grafica-barra',
@@ -8,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  public barChartOptions: ChartOptions = {
+    responsive: true
+  };
+  public barChartLabels: Label[] = ['2021', '2022', '2023', '2024', '2025', '2026', '2027'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#7700FF', hoverBackgroundColor: '#ec0b0b' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#0C83E8', hoverBackgroundColor: '#ec0b0b' },
+    { data: [0, 30, 4, 29, 56, 47, 60], label: 'Series C', backgroundColor: '#0DFF73', hoverBackgroundColor: '#ec0b0b' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
