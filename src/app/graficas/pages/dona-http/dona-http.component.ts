5e349af041538066f84f0adee2acb5af0d6c2e7c
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-19 17:40:15 +0200

    change data with rxjs (lesson 340 course angular)

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -34,14 +34,19 @@ export class DonaHttpComponent implements OnInit {
   constructor( private graficasService: GraficasService) { }
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales()
-      .subscribe(data => {
-        console.log(data)
-        const labels =  Object.keys(data);
-        const values =  Object.values(data);
-
+    // this.graficasService.getUsuariosRedesSociales()
+    //   .subscribe(data => {
+    //     console.log(data)
+    //     const labels =  Object.keys(data);
+    //     const values =  Object.values(data);
+    //
+    //     this.doughnutChartLabels = labels;
+    //     this.doughnutChartData.push(values);
+    //   })
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+      .subscribe(({labels,values}) => {
         this.doughnutChartLabels = labels;
-        this.doughnutChartData.push(values);
+        this.doughnutChartData.push(values)
       })
   }
 
