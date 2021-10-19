dcdc924aa6a2621193dbdba9c98c756f1eaacad7
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-19 16:32:43 +0200

    show data in the component chart (lesson 339 course angular)

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GraficasService} from "../../services/graficas.service";
+import {Color, Label, MultiDataSet} from "ng2-charts";
+import {ChartType} from "chart.js";
 
 @Component({
   selector: 'app-dona-http',
@@ -9,12 +11,37 @@ import {GraficasService} from "../../services/graficas.service";
 })
 export class DonaHttpComponent implements OnInit {
 
+  // Doughnut
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 150]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+    backgroundColor: [
+      '#FA0DA5',
+      '#9F01F5',
+      '#CE0BDE',
+      '#5B0BDE',
+      '#250DFA',
+    ]
+  }]
+
   constructor( private graficasService: GraficasService) { }
 
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales()
       .subscribe(data => {
         console.log(data)
+        const labels =  Object.keys(data);
+        const values =  Object.values(data);
+
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push(values);
       })
   }
 
